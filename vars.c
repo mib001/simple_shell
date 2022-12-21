3fddf497b24a9e590fac204f757923ce625f12f6
Izakti11 <113716030+Izakti11@users.noreply.github.com>
2022-12-21 21:31:36 +0300

    Update vars.c

--- vars.c
+++ vars.c
@@ -75,7 +75,7 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
  *
  * Return: 1 if replaced, 0 otherwise
  */
-int replace_alias(info_t *info)
+int replace_alias(ino_t *info)
 {
 	int i;
 	list_t *node;
@@ -104,7 +104,7 @@ int replace_alias(info_t *info)
  *
  * Return: 1 if replaced, 0 otherwise
  */
-int replace_vars(info_t *info)
+int replace_vars(ino_t *info)
 {
 	int i = 0;
 	list_t *node;
